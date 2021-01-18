44dfa6b77e7320cdd0e17a7ced60462749ba288a
Mark-Kohrman <mark.kohrman@gmail.com>
2021-01-17 21:41:30 -0600

    finish react tutorial

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,32 +2,34 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = { 
-    count: 0 ,
-    tags: []
+    count: 0,
+   
   };
 
 
-  handleIncrement = () => {
-    console.log('Increment Clicked', this);
+  handleIncrement = (product) => {
+    console.log(product)
+     this.setState( { count: this.state.count + 1 } );
 
   }
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>
-    return  <ul> { this.state.tags.map(tag => <li key={tag}>{ tag }</li>) } </ul>
-  }
-
-   
   
+//  doHandleIncrement = () => {
+//    this.handleIncrement({id: 1})
+//  }
+
 
   render() {
 
     this.getBadgeClasses();
-
+ 
     return (
     <div>
       <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-      <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+      <button 
+        onClick={ () => this.handleIncrement(product) }
+        className="btn btn-secondary btn-sm">
+        Increment
+        </button>
     </div>
     );
   } 
@@ -35,6 +37,7 @@ class Counter extends Component {
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
   }
 
   formatCount(){
